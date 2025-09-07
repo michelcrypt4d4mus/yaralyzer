1f5108218d2e85a75a51f20cbbb176080b4186c3
ashariyar <ashariyar@users.noreply.github.com>
2025-09-07 17:21:45 -0400

    add submodules

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -10,7 +10,13 @@ YARALYZER_PKG="yaralyzer"
 lazydocs --output-path docs/api \
          --overview-file="README.md" \
          --src-base-url="https://github.com/michelcrypt4d4mus/$YARALYZER_PKG/blob/main/" \
-         "$YARALYZER_PKG"
+         "$YARALYZER_PKG" \
+         "$YARALYZER_PKG/decoding" \
+         "$YARALYZER_PKG/encoding_detection" \
+         "$YARALYZER_PKG/helpers" \
+         "$YARALYZER_PKG/output" \
+         "$YARALYZER_PKG/util" \
+         "$YARALYZER_PKG/yara"
 
 mkdocs build
 # mkdocs serve

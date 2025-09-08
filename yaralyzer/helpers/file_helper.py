671407b233f2d58f99ba35b68faefa3e6573c348
ashariyar <ashariyar@users.noreply.github.com>
2025-09-08 00:44:50 -0400

    docstrings

--- yaralyzer/helpers/file_helper.py
+++ yaralyzer/helpers/file_helper.py
@@ -12,7 +12,13 @@ def timestamp_for_filename() -> str:
 
 
 def files_in_dir(dir: str, with_extname: Optional[str] = None) -> List[str]:
-    """paths for non dot files, optionally ending in 'with_extname'."""
+    """
+    Paths for non dot files, optionally ending in 'with_extname'.
+
+    Args:
+        dir (str): Directory to list files from.
+        with_extname (Optional[str], optional): If set, only return files with this extension. Defaults to None.
+    """
     files = [path.join(dir, path.basename(file)) for file in listdir(dir) if not file.startswith('.')]
     files = [file for file in files if not path.isdir(file)]
 
@@ -23,6 +29,7 @@ def files_in_dir(dir: str, with_extname: Optional[str] = None) -> List[str]:
 
 
 def files_with_extname(files: List[str], extname: str) -> List[str]:
+    """Return only files from the list that end with the given extname."""
     return [f for f in files if f.endswith(f".{extname}")]
 
 
@@ -33,10 +40,12 @@ def load_word_list(file_path):
 
 
 def load_binary_data(file_path) -> bytes:
+    """Load and return the raw bytes from a file."""
     with open(file_path, 'rb') as f:
         return f.read()
 
 
 def load_file(file_path) -> str:
+    """Load and return the text contents of a file."""
     with open(file_path, 'r') as f:
         return f.read()

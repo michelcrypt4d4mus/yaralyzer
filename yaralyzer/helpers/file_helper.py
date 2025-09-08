4cc138f3843fccfe0542821e3b3b19768efb768e
ashariyar <ashariyar@users.noreply.github.com>
2025-09-08 01:18:22 -0400

    alphabetize; remove unused load_word_list()

--- yaralyzer/helpers/file_helper.py
+++ yaralyzer/helpers/file_helper.py
@@ -3,15 +3,11 @@ Helper methods to work with files.
 """
 from datetime import datetime
 from os import listdir, path
+from pathlib import Path
 from typing import List, Optional
 
 
-def timestamp_for_filename() -> str:
-    """Returns a string showing current time in a file name friendly format."""
-    return datetime.now().strftime("%Y-%m-%dT%H.%M.%S")
-
-
-def files_in_dir(dir: str, with_extname: Optional[str] = None) -> List[str]:
+def files_in_dir(dir: Path | str, with_extname: Optional[str] = None) -> List[str]:
     """
     Paths for non dot files, optionally ending in 'with_extname'.
 
@@ -33,19 +29,18 @@ def files_with_extname(files: List[str], extname: str) -> List[str]:
     return [f for f in files if f.endswith(f".{extname}")]
 
 
-def load_word_list(file_path):
-    """For very simple files (1 col CSVs, if you will)."""
-    with open(file_path, 'r') as f:
-        return [line.rstrip().lstrip() for line in f.readlines()]
-
-
-def load_binary_data(file_path) -> bytes:
+def load_binary_data(file_path: Path | str) -> bytes:
     """Load and return the raw bytes from a file."""
     with open(file_path, 'rb') as f:
         return f.read()
 
 
-def load_file(file_path) -> str:
+def load_file(file_path: Path | str) -> str:
     """Load and return the text contents of a file."""
     with open(file_path, 'r') as f:
         return f.read()
+
+
+def timestamp_for_filename() -> str:
+    """Returns a string showing current time in a file name friendly format."""
+    return datetime.now().strftime("%Y-%m-%dT%H.%M.%S")

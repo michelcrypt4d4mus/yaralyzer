a0e74f714b7a56b7ff087470d1971248a7c6d0d2
ashariyar <ashariyar@users.noreply.github.com>
2026-01-17 20:21:52 -0500

    JustifyMethod type hint

--- yaralyzer/output/file_hashes_table.py
+++ yaralyzer/output/file_hashes_table.py
@@ -5,6 +5,7 @@ import hashlib
 from collections import namedtuple
 from typing import Optional, Union
 
+from rich.console import JustifyMethod
 from rich.table import Column, Table
 
 from yaralyzer.helpers.rich_text_helper import LEFT, size_text
@@ -16,7 +17,7 @@ BytesInfo = namedtuple('BytesInfo', ['size', 'md5', 'sha1', 'sha256'])
 def bytes_hashes_table(
     bytes_or_bytes_info: Union[bytes, BytesInfo],
     title: Optional[str] = None,
-    title_justify: str = LEFT
+    title_justify: JustifyMethod = LEFT
 ) -> Table:
     """
     Build a Rich `Table` displaying the size, MD5, SHA1, and SHA256 hashes of a byte sequence.
@@ -25,7 +26,7 @@ def bytes_hashes_table(
         bytes_or_bytes_info (Union[bytes, BytesInfo]): The `bytes` to hash, or a `BytesInfo`
             namedtuple with precomputed values.
         title (Optional[str], optional): Optional title for the table. Defaults to `None`.
-        title_justify (str, optional): Justification for the table title. Defaults to `"LEFT"`.
+        title_justify (JustifyMethod, optional): Justification for the table title. Defaults to `"LEFT"`.
 
     Returns:
         Table: A Rich `Table` object with the size and hash values.

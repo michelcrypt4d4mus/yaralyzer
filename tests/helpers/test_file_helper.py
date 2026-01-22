094d203bc0ae1354448d95f09da6c896bcf37b59
ashariyar <ashariyar@users.noreply.github.com>
2026-01-22 16:17:51 -0500

    test

--- tests/helpers/test_file_helper.py
+++ tests/helpers/test_file_helper.py
@@ -1,11 +1,13 @@
-from os.path import dirname
+from os.path import dirname, join
 
 from yaralyzer.helpers.file_helper import files_in_dir
 
 
 def test_files_in_dir():
-    this_dir_files = files_in_dir(dirname(__file__))
+    this_dir = dirname(__file__)
+    this_dir_files = files_in_dir(this_dir)
     assert len(this_dir_files) >= 3
     assert __file__ in this_dir_files
-    assert len(files_in_dir(dirname(__file__), with_extname='illmatic')) == 0
-    assert len(files_in_dir(dirname(__file__), with_extname='py')) >= 3
+    assert len(files_in_dir(this_dir, with_extname='illmatic')) == 0
+    assert len(files_in_dir(this_dir, with_extname='py')) >= 3
+    assert join(this_dir, 'test_rich_text_helper.py') in [str(f) for f in this_dir_files]

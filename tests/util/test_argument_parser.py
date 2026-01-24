b09aa49170cc4f1c6fbc3368575f5927b6e7e369
ashariyar <ashariyar@users.noreply.github.com>
2026-01-24 02:28:37 -0500

    use temporary_env() in tests

--- tests/util/test_argument_parser.py
+++ tests/util/test_argument_parser.py
@@ -6,29 +6,14 @@ from os import environ
 from yaralyzer.config import YaralyzerConfig
 from yaralyzer.util.argument_parser import parse_arguments
 from yaralyzer.util.constants import YARALYZE
+from yaralyzer.util.helpers.env_helper import temporary_env
 
 ENV_VARS_TO_SUSPEND = []
 BASE_ARGV = [YARALYZE, __file__, '-Y', 'rules.yara']
 
 
-@contextmanager
-def setup_and_tear_down_env_vars(env_vars: dict):
-    # Will be executed before the first test
-    old_environ = dict(environ)
-    environ.update(env_vars)
-
-    for env_var in ENV_VARS_TO_SUSPEND:
-        environ.pop(env_var, default=None)
-
-    yield
-
-    # Will be executed after the last test
-    environ.clear()
-    environ.update(old_environ)
-
-
 def test_env_var_bool():
-    with setup_and_tear_down_env_vars({'YARALYZER_SUPPRESS_DECODES_TABLE': 'True'}):
+    with temporary_env({'YARALYZER_SUPPRESS_DECODES_TABLE': 'True'}):
         sys.argv = BASE_ARGV
         parse_arguments()
         assert YaralyzerConfig.args.min_chardet_bytes == 9
@@ -36,14 +21,14 @@ def test_env_var_bool():
 
 
 def test_env_var_int():
-    with setup_and_tear_down_env_vars({'YARALYZER_SURROUNDING_BYTES': '202'}):
+    with temporary_env({'YARALYZER_SURROUNDING_BYTES': '202'}):
         sys.argv = BASE_ARGV
         parse_arguments()
         assert YaralyzerConfig.args.surrounding_bytes == 202
 
 
 def test_cli_overrides_env():
-    with setup_and_tear_down_env_vars({'YARALYZER_SURROUNDING_BYTES': '202'}):
+    with temporary_env({'YARALYZER_SURROUNDING_BYTES': '202'}):
         sys.argv = BASE_ARGV + ['--surrounding-bytes', '123']
         parse_arguments()
         assert YaralyzerConfig.args.surrounding_bytes == 123

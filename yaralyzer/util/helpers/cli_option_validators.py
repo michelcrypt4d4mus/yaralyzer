176a0066794b78bd7bbdb886963421cb88922204
ashariyar <ashariyar@users.noreply.github.com>
2026-01-25 13:46:45 -0500

    argument validators

---
+++ yaralyzer/util/helpers/cli_option_validators.py
@@ -0,0 +1,62 @@
+"""
+Validators for command line arguments.
+"""
+import re
+from argparse import ArgumentTypeError
+from pathlib import Path
+
+import yara
+
+from yaralyzer.yara.yara_rule_builder import PATTERN_TYPES, build_yara_rule
+
+
+class DirArgValidator:
+    def __call__(self, value: str) -> Path:
+        dir = Path(value)
+
+        if not dir.exists():
+            raise ArgumentTypeError(f"'{dir}' is not a directory that exists")
+        elif not dir.is_dir():
+            raise ArgumentTypeError(f"'{dir}' is a file not directory")
+
+        return dir
+
+
+class FileArgValidator:
+    def __call__(self, value: str) -> Path:
+        file_path = Path(value)
+
+        if not file_path.exists():
+            raise ArgumentTypeError(f"'{file_path}' is not a file that exists")
+
+        return file_path
+
+
+class PatternsLabelValidator:
+    PATTERN_LABEL_REGEX = re.compile(r"^\w+$")
+
+    def __call__(self, value: str) -> str:
+        if not self.PATTERN_LABEL_REGEX.match(value):
+            raise ArgumentTypeError('--patterns-label can only include alphanumeric chars and underscore')
+
+        return value
+
+
+class YaraRegexValidator:
+    def __call__(self, value: str) -> str:
+        compiled_rule = None
+        compilation_error = None
+
+        # Since we can't know if it's a hex or regex pattern yet, try both and accept if either works.
+        for pattern_type in PATTERN_TYPES:
+            try:
+                compiled_rule = build_yara_rule(value, pattern_type=pattern_type)
+            except yara.SyntaxError as e:
+                compilation_error = e
+            except Exception as e:
+                compilation_error = e
+
+        if compiled_rule:
+            return value
+        else:
+            raise ArgumentTypeError(f"SyntaxError: your pattern is invalid ({compilation_error})")

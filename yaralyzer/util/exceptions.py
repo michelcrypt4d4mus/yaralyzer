2bd82680f97620867ff7ac1d5a661f8d6f31c400
ashariyar <ashariyar@users.noreply.github.com>
2026-01-26 18:04:01 -0500

    don't exit if missing cairosvg when checking args

--- yaralyzer/util/exceptions.py
+++ yaralyzer/util/exceptions.py
@@ -2,6 +2,7 @@ from argparse import ArgumentTypeError
 from rich.padding import Padding
 from rich.text import Text
 
+from yaralyzer.util.helpers.env_helper import is_invoked_by_pytest
 from yaralyzer.util.logging import highlighter, log_console
 
 
@@ -11,7 +12,7 @@ class InvalidArgumentError(ArgumentTypeError):
 
 def handle_argument_error(msg: str, e: Exception | None = None, is_standalone_mode: bool = False) -> None:
     """Standalone mode means in a situation where the `yaralyze` command is being run."""
-    if is_standalone_mode:
+    if is_standalone_mode and not is_invoked_by_pytest():
         print_fatal_error_and_exit(msg, e)
     else:
         raise e or InvalidArgumentError(msg)
